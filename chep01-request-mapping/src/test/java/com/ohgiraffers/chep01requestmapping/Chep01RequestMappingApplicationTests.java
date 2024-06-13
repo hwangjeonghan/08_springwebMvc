0d14a5bc4198c3f646da2cd5c60647f6205d5833
hwang <hjeonghan0@gmail.com>
2024-06-13 17:32:46 +0900

    first commit

---
+++ chep01-request-mapping/src/test/java/com/ohgiraffers/chep01requestmapping/Chep01RequestMappingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.chep01requestmapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chep01RequestMappingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
